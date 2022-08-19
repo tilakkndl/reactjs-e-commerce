fffc6c6e7a6cfb30fc408b49ff5a026ef6ae8d7a
Tilak Kandel <tilakkndl2001@gmail.com>
2022-08-19 15:51:13 +0545

    Created app with create react app and added file for the home component

---
+++ src/home.component.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./home.style.scss";
+export const Homepage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HAT</h1>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">JACKETS</h1>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SNEAKERS</h1>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WOMENS</h1>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MENS</h1>
+          <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
